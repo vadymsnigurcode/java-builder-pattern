e350c79a02f33231ca39c9e3a8f7fda55b9ea619
Vadym <vadym.snigur.code@gmail.com>
2022-02-01 20:45:56 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,31 @@
+import builders.CarBuilder;
+import builders.CarManualBuilder;
+import cars.Car;
+import cars.Manual;
+import director.Director;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Director director = new Director();
+
+        // Директор получает объект конкретного строителя от клиента
+        // (приложения). Приложение само знает какой строитель использовать,
+        // чтобы получить нужный продукт.
+        CarBuilder builder = new CarBuilder();
+        director.constructSportsCar(builder);
+
+        // Готовый продукт возвращает строитель, так как Директор чаще всего не
+        // знает и не зависит от конкретных классов строителей и продуктов.
+        Car car = builder.getResult();
+        System.out.println("Car built:\n" + car.getCarType());
+
+
+        CarManualBuilder manualBuilder = new CarManualBuilder();
+
+        // Директор может знать больше одного рецепта строительства.
+        director.constructSportsCar(manualBuilder);
+        Manual carManual = manualBuilder.getResult();
+        System.out.println("\nCar manual built:\n" + carManual.print());
+    }
+}
---
+++ src/main/java/builders/Builder.java
@@ -0,0 +1,20 @@
+package builders;
+
+import cars.CarType;
+import components.Engine;
+import components.GPSNavigator;
+import components.Transmission;
+import components.TripComputer;
+
+/**
+ * Интерфейс Строителя объявляет все возможные этапы и шаги конфигурации
+ * продукта.
+ */
+public interface Builder {
+    void setCarType(CarType type);
+    void setSeats(int seats);
+    void setEngine(Engine engine);
+    void setTransmission(Transmission transmission);
+    void setTripComputer(TripComputer tripComputer);
+    void setGPSNavigator(GPSNavigator gpsNavigator);
+}
